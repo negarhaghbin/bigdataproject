8bce6bdf8e9d6a68d95cf03ff26efa1a42a5bc2e
Negar <nbeanm@gmail.com>
2019-04-06 12:29:52 -0400

    data_prepration2 added

--- include/KFoldCV.py
+++ include/KFoldCV.py
@@ -1,9 +1,14 @@
-from include.dataPrepration import data_preparation
+from include.dataPrepration import data_preparation,data_preparation2
 from sklearn.model_selection import cross_val_score
+import numpy as np
 
 
 def KFoldCV(datafiles,alg):
-    X,y= data_preparation(datafiles)
-    # for 10-fold
-    score = cross_val_score(alg, X, y, cv=10, scoring='f1_micro')
-    print(score.mean())
+    scorList=[]
+    for i in np.arange(0.25, 7.25, 0.25):
+        # X,y= data_preparation(datafiles,i)
+        X, y = data_preparation2(datafiles, i)
+        # for 10-fold
+        score = cross_val_score(alg, X, y, cv=10, scoring='f1_micro')
+        scorList.append((score.mean(),i))
+    print(scorList)
--- include/dataPrepration.py
+++ include/dataPrepration.py
@@ -42,7 +42,7 @@ def mean(x):
 
 
 # (window_size,list of(list of(windows)))
-def data_preparation(filenames):
+def data_preparation(filenames, window_size):
     """
     in this function we first remove the data which doesnt have label and then only select timestamp, 9 sensors
     acceleration and label columns.
@@ -50,26 +50,60 @@ def data_preparation(filenames):
     sc = init_spark().sparkContext
     X=sc.parallelize([])
     for filename in filenames:
-        for i in np.arange(0.25, 7, 0.25):
-            data_count = int(i / timestamp)
-            rdd = sc.textFile(filename) \
+        data_count = int(window_size / timestamp)
+        rdd = sc.textFile(filename) \
                 .map(lambda row: row.split())
-            X_temp = rdd.map(lambda x: ([float(x[2]), float(x[3]), float(x[4])
-                                            , float(x[15]), float(x[16]), float(x[17])
-                                            , float(x[28]), float(x[29]), float(x[30])
-                                            , float(x[41]), float(x[42]), float(x[43])
-                                            , float(x[54]), float(x[55]), float(x[56])
-                                            , float(x[67]), float(x[68]), float(x[69])
-                                            , float(x[80]), float(x[81]), float(x[82])
-                                            , float(x[93]), float(x[94]), float(x[95])
-                                            , float(x[106]), float(x[107]), float(x[108])],
+        X_temp = rdd.map(lambda x: ([float(x[2]), float(x[3]), float(x[4])
+                                    , float(x[15]), float(x[16]), float(x[17])
+                                    , float(x[28]), float(x[29]), float(x[30])
+                                    , float(x[41]), float(x[42]), float(x[43])
+                                    , float(x[54]), float(x[55]), float(x[56])
+                                    , float(x[67]), float(x[68]), float(x[69])
+                                    , float(x[80]), float(x[81]), float(x[82])
+                                    , float(x[93]), float(x[94]), float(x[95])
+                                    , float(x[106]), float(x[107]), float(x[108])],
                                         [int(x[119])])).zipWithIndex()
-            X_temp = X_temp.map(lambda x: (int(x[1] / data_count), x[0])) \
+        X_temp = X_temp.map(lambda x: (int(x[1] / data_count), x[0])) \
                 .map(lambda x: (x[0], (x[1], 1)))\
                 .reduceByKey(lambda x, y: addList(x, y)) \
                 .map(lambda x: mean(x[1]))
 
         X=X.union(X_temp)
+
+    y = X.map(lambda x: x[1])
+    X = X.map(lambda x: x[0])
+    return np.array(X.collect()).astype(np.float64), np.array(y.collect()).astype(np.float64)
+
+def data_preparation2(filenames, window_size):
+    """
+    in this function we first remove the data which doesnt have label and then only select timestamp, 9 sensors
+    acceleration and label columns.
+    """
+    sc = init_spark().sparkContext
+    X=sc.parallelize([])
+    rdd = sc.parallelize([])
+    for filename in filenames:
+        data_count = int(window_size / timestamp)
+        rdd_temp = sc.textFile(filename) \
+                .map(lambda row: row.split())
+        rdd=rdd.union(rdd_temp)
+
+    X_temp = rdd.map(lambda x: ([float(x[2]), float(x[3]), float(x[4])
+                                    , float(x[15]), float(x[16]), float(x[17])
+                                    , float(x[28]), float(x[29]), float(x[30])
+                                    , float(x[41]), float(x[42]), float(x[43])
+                                    , float(x[54]), float(x[55]), float(x[56])
+                                    , float(x[67]), float(x[68]), float(x[69])
+                                    , float(x[80]), float(x[81]), float(x[82])
+                                    , float(x[93]), float(x[94]), float(x[95])
+                                    , float(x[106]), float(x[107]), float(x[108])],
+                                        [int(x[119])])).zipWithIndex()
+    X_temp = X_temp.map(lambda x: (int(x[1] / data_count), x[0])) \
+                .map(lambda x: (x[0], (x[1], 1)))\
+                .reduceByKey(lambda x, y: addList(x, y)) \
+                .map(lambda x: mean(x[1]))
+
+    X=X.union(X_temp)
 
     y = X.map(lambda x: x[1])
     X = X.map(lambda x: x[0])
