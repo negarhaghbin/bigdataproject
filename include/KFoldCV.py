a726f31be3c1ceb35a51affbf6132a7a8d7f9c7c
Negar <nbeanm@gmail.com>
2019-04-07 23:14:03 -0400

    plot added

--- include/KFoldCV.py
+++ include/KFoldCV.py
@@ -1,6 +1,7 @@
 from include.dataPrepration import data_preparation,data_preparation2
 from sklearn.model_selection import cross_val_score
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def KFoldCV(datafiles,alg):
@@ -12,3 +13,13 @@ def KFoldCV(datafiles,alg):
         score = cross_val_score(alg, X, y, cv=10, scoring='f1_micro')
         scorList.append((score.mean(),i))
     print(scorList)
+    resultScore = []
+    resultWindow = []
+    for i in range(0, len(scorList)):
+        resultScore.append(scorList[i][0])
+        resultWindow.append((i + 1) * 0.25)
+    plt.plot(resultWindow, resultScore)
+    plt.ylim(0.2, 1)
+    plt.xlabel('window sizes')
+    plt.ylabel('f1-scores')
+    plt.show()
